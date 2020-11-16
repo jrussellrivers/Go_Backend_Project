f546e91c2e293b5319617d98a7d6cc8fd1ea862c
Jordan Rivers <j.russell.rivers@gmail.com>
2020-11-16 14:20:12 -0500

    user pool created for multi users

---
+++ backend/pkg/websocket/pool.go
@@ -0,0 +1,53 @@
+package websocket
+
+import "fmt"
+
+// Pool structure
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+// NewPool creates a pool
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+// Start will listen for anything passed to any of our Pool's channels
+// then if anything is received into one of these channels, it’ll act accordingly.
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			for client := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			}
+			break
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			for client := range pool.Clients {
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+			}
+			break
+		case message := <-pool.Broadcast:
+			fmt.Println("Sending message to all clients in Pool")
+			for client := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		}
+	}
+}
